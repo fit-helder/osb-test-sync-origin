0c69b219df5d06d061ca5223bc64b50447863749
“fit-helder” <helder.lima@fitbank.com.br>
2023-08-18 16:15:11 -0300

    feat: tesyte13

--- pipelines-sync/sync-commit.sh
+++ pipelines-sync/sync-commit.sh
@@ -5,8 +5,6 @@ echo "Current Branch: $BRANCH_NAME"
 REPO_ORIGEM="https://github.com/fit-helder/osb-test-sync-origin.git"
 REPO_DESTINO="https://github.com/fit-helder/osb-test-sync-wl1.git"
 
-git clone $REPO_ORIGEM .
-
 git remote add destino $REPO_DESTINO
 
 git push destino $BRANCH_NAME
