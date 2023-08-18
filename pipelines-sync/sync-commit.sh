91ac1c63a9666db851d8c1b5b7d1130df0ff80d4
“fit-helder” <helder.lima@fitbank.com.br>
2023-08-18 15:50:04 -0300

    feat: teste6

--- pipelines-sync/sync-commit.sh
+++ pipelines-sync/sync-commit.sh
@@ -4,7 +4,7 @@ echo "Current Branch: $(Build.SourceBranchName)"
 
 REPO_ORIGEM="https://github.com/fit-helder/osb-test-sync-origin.git"
 REPO_DESTINO="https://github.com/fit-helder/osb-test-sync-wl1.git"
-BRANCH=$(Build.SourceBranchName)
+BRANCH=${BRANCH}
 
 git clone $REPO_ORIGEM repo-temp
 cd repo-temp
