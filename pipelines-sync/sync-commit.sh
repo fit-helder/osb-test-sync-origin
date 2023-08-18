033f6d4b21d60aac2b767c1a7b3ee29dcceffdff
“fit-helder” <helder.lima@fitbank.com.br>
2023-08-18 16:54:58 -0300

    teste

--- pipelines-sync/sync-commit.sh
+++ pipelines-sync/sync-commit.sh
@@ -5,10 +5,13 @@ echo "token: $GITHUB_TOKEN"
 
 REPO_DESTINO="https://$GITHUB_TOKEN@github.com/fit-helder/osb-test-sync-wl1.git"
 
+git config user.email "helder.lima@fitbank.com.br"
+git config user.name "fit-helder"
+
+git remote add destino $REPO_DESTINO
+
 git add .
 
 git commit -m 'update from source'
-
-git remote add destino $REPO_DESTINO
 
 git push https://$GITHUB_TOKEN@github.com/fit-helder/osb-test-sync-wl1.git $BRANCH_NAME
